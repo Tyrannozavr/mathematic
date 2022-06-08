95832f59fc198512df3e9ce8e354156aeaeb34ce
dmiv <dmitriy40647274@gmail.com>
2022-06-08 23:39:39 +0300

    first commit

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,30 +1,64 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+def solve(a, b, c):
+    a, b, c = int(a), int(b), int(c)
+    D = b**2 - 4 * a * c
+    if D < 0:
+        return '- has no solutions'
+    elif D == 0:
+        return f'= {-b / (2 * a)}'
+    else:
+        x1 = (-b - D**(1/2)) / (2 * a)
+        x2 = (-b + D**(1/2)) / (2 * a)
+        return f'= {x1} or {x2}'
 
 def index(request):
-    unknown_variables = ['a', 'b', 'c']
-    message = f"please, change variables: {', '.join(unknown_variables)}"
+    a, b, c, = request.session.get('a', 'a'), request.session.get('b', 'b'), request.session.get('c', 'c')
+    answer = ''
+    if request.session['variables']:
+        unknown_variables = request.session.get('variables')
+    else:
+        unknown_variables = ['a', 'b', 'c']
+        request.session['variables'] = ['a', 'b', 'c']
+    if unknown_variables[0] == 'full':
+        answer = solve(a, b, c)
+        message = ''
+    else:
+        message = f"please, change variables: {', '.join(unknown_variables)}"
     content = {
-        'a': 'a',
-        'b': 'b',
-        'c': 'c',
+        'a': a,
+        'b': b,
+        'c': c,
         'message': message,
-    }
+        'answer': answer}
     if request.POST:
         a = request.POST.get('a', None)
         b = request.POST.get('b', None)
         c = request.POST.get('c', None)
         if a:
-            content['a'] = a
-            unknown_variables.remove('a')
-            content['message'] = f"please, change variables: {', '.join(unknown_variables)}"
+            request.session['a'] = a
+            try:
+                unknown_variables.remove('a')
+            except ValueError:
+                print('remove a')
         if b:
-            content['b'] = b
-            unknown_variables.remove('b')
-            content['message'] = f"please, change variables: {', '.join(unknown_variables)}"
+            request.session['b'] = b
+            try:
+                unknown_variables.remove('b')
+            except ValueError:
+                print('remove b')
         if c:
-            content['c'] = c
-            unknown_variables.remove('c')
+            request.session['c'] = c
+            try:
+                unknown_variables.remove('c')
+            except ValueError:
+                print('remove c')
+        if len(unknown_variables) > 1:
             content['message'] = f"please, change variables: {', '.join(unknown_variables)}"
-        return render(request, 'index.html', content)
+        else:
+            content['message'] = f"please, change variable: {', '.join(unknown_variables)}"
+        if len(request.session.get('variables')) == 0:
+            request.session['variables'] = ['full']
+        return redirect('/')
+    request.session['variables'] = request.session['variables']
     return render(request, 'index.html', content)
